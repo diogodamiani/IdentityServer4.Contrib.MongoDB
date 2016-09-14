5ee4b7674d8e748bfdd61e609f37985339cec63e
Diogo Damiani <diogodamiani@gmail.com>
2016-09-14 16:49:39 -0300

    Add builder extensions

---
+++ src/IdentityServer4.MongoDB/Extensions/IdentityServerMongoDBBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.MongoDB.Configuration;
+using IdentityServer4.MongoDB.DbContexts;
+using IdentityServer4.MongoDB.Interfaces;
+using IdentityServer4.MongoDB.Services;
+using IdentityServer4.MongoDB.Stores;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class IdentityServerMongoDBBuilderExtensions
+    {
+        public static IIdentityServerBuilder AddConfigurationStore(
+            this IIdentityServerBuilder builder, IConfiguration configuration)
+        {
+            builder.Services.AddOptions();
+            builder.Services.Configure<MongoDBConfiguration>(configuration.GetSection("MongoDB"));
+
+            builder.Services.AddScoped<IConfigurationDbContext, ConfigurationDbContext>();
+
+            builder.Services.AddTransient<IClientStore, ClientStore>();
+            builder.Services.AddTransient<IScopeStore, ScopeStore>();
+            builder.Services.AddTransient<ICorsPolicyService, CorsPolicyService>();
+
+            return builder;
+        }
+
+        public static IIdentityServerBuilder AddOperationalStore(
+            this IIdentityServerBuilder builder, IConfiguration configuration)
+        {
+            builder.Services.AddOptions();
+            builder.Services.Configure<MongoDBConfiguration>(configuration.GetSection("MongoDB"));
+
+            builder.Services.AddScoped<IPersistedGrantDbContext, PersistedGrantDbContext>();
+
+            builder.Services.AddTransient<IPersistedGrantStore, PersistedGrantStore>();
+
+            return builder;
+        }
+    }
+}

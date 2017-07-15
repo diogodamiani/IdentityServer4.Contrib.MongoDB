a5747c277562df313dd11273c1f832dacf888034
kutlueren <kutlu_eren26@hotmail.com>
2017-07-15 11:56:28 +0300

    user data access added

---
+++ src/Host/DataAccess/UserDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Host.DataAccess
+{
+    public class UserDataAccess
+    {
+    }
+}

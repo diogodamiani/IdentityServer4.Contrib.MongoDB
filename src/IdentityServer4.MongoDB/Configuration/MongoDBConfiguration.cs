393ea53a11a766f91b9f736ee1d405f72de4a8ed
Guy Lepine <delepster@gmail.com>
2017-10-13 14:34:40 -0400

    added SSL settigns to MongoDBConfiguration

--- src/IdentityServer4.MongoDB/Configuration/MongoDBConfiguration.cs
+++ src/IdentityServer4.MongoDB/Configuration/MongoDBConfiguration.cs
@@ -1,8 +1,11 @@
-﻿namespace IdentityServer4.MongoDB.Configuration
+﻿using MongoDB.Driver;
+
+namespace IdentityServer4.MongoDB.Configuration
 {
     public class MongoDBConfiguration
     {
         public string ConnectionString { get; set; }
         public string Database { get; set; }
+        public SslSettings SslSettings { get; set; }
     }
 }

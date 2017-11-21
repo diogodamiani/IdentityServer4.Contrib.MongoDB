939def84cf20a33b175c641c7666df4e1f82d5df
Diogo Damiani <diogodamiani@gmail.com>
2017-11-21 15:14:13 -0200

    Update package name to IdentityServer4.Contrib.MongoDB

--- src/IdentityServer4.MongoDB/Properties/AssemblyInfo.cs
+++ src/IdentityServer4.MongoDB/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IdentityServer4.MongoDB")]
+[assembly: AssemblyProduct("IdentityServer4.Contrib.MongoDB")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible

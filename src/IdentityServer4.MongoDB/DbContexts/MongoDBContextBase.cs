b611f7086e343f51f9fde9e84199cbe8d20e4a90
Guy Lepine <delepster@gmail.com>
2018-09-06 13:10:01 -0400

    Added SSL settings to MongoDBConfiguration (#12)
    * added SSL settigns to MongoDBConfiguration
    * use SSL settings when create MongoDB client
    * added flag to use SSL

--- src/IdentityServer4.MongoDB/DbContexts/MongoDBContextBase.cs
+++ src/IdentityServer4.MongoDB/DbContexts/MongoDBContextBase.cs
@@ -21,7 +21,21 @@ namespace IdentityServer4.MongoDB.DbContexts
             if (settings.Value.Database == null)
                 throw new ArgumentNullException(nameof(settings), "MongoDBConfiguration.Database cannot be null.");
 
-            _client = new MongoClient(settings.Value.ConnectionString);
+
+            var clientSettings = MongoClientSettings.FromUrl(new MongoUrl(settings.Value.ConnectionString));
+            
+            if (clientSettings.SslSettings != null)
+            {
+                clientSettings.SslSettings = settings.Value.SslSettings;
+                clientSettings.UseSsl = true;
+            }
+            else
+            {
+                clientSettings.UseSsl = false;
+            }
+
+            _client = new MongoClient(clientSettings);
+
             _database = _client.GetDatabase(settings.Value.Database);
         }
 

718f88d5d887228ec59e4cf3e62eb3ff4c952d03
Diogo Damiani <diogodamiani@gmail.com>
2020-08-02 15:28:15 -0300

    Mongo Connection Stuff (#27)
    * Allow using MongoDb connectionString including DatabaseName (#22)
    
    * create MongoClient based on complete MongoClientSettings (#25)
    
    * Fix DatabaseName validation
    
    * Change version to 2.3.4
    
    Co-authored-by: Artem Repko <roby-boby@ukr.net>
    Co-authored-by: Yan Oreshchenkov <yan-oreshchenkov@users.noreply.github.com>

--- src/IdentityServer4.MongoDB/DbContexts/MongoDBContextBase.cs
+++ src/IdentityServer4.MongoDB/DbContexts/MongoDBContextBase.cs
@@ -17,10 +17,12 @@ namespace IdentityServer4.MongoDB.DbContexts
             if (settings.Value.ConnectionString == null)
                 throw new ArgumentNullException(nameof(settings), "MongoDBConfiguration.ConnectionString cannot be null.");
 
-            if (settings.Value.Database == null)
+            var mongoUrl = MongoUrl.Create(settings.Value.ConnectionString);
+
+            if (settings.Value.Database == null && mongoUrl.DatabaseName == null)
                 throw new ArgumentNullException(nameof(settings), "MongoDBConfiguration.Database cannot be null.");
 
-            var clientSettings = MongoClientSettings.FromUrl(new MongoUrl(settings.Value.ConnectionString));
+            var clientSettings = MongoClientSettings.FromUrl(mongoUrl);
 
             if (clientSettings.SslSettings != null)
             {
@@ -32,8 +34,8 @@ namespace IdentityServer4.MongoDB.DbContexts
                 clientSettings.UseSsl = false;
             }
 
-            _client = new MongoClient(settings.Value.ConnectionString);
-            Database = _client.GetDatabase(settings.Value.Database);
+            _client = new MongoClient(clientSettings);
+            Database = _client.GetDatabase(settings.Value.Database ?? mongoUrl.DatabaseName);
         }
 
         protected IMongoDatabase Database { get; }
